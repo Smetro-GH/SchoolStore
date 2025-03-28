71745460f2dbe5a16cdfcb4bf77b37c740aeb469
Francisco <juanfglezc2008@outlook.com>
2025-03-28 02:05:06 -0700

    skibidi

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+    return render_template("login.html")
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return '<p>signup</p>'
+    return render_template("signup.html")
 
 @auth.route('/logout', methods=['GET', 'POST'])
 def logout():
